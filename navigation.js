be1d3d479661c7e539697a7cf4e252acc5a3d884
Brice Shatzer <brice.shatzer@gmail.com>
2014-09-20 13:32:43 -0500

    Added site name label to navigation bar

--- navigation.js
+++ navigation.js
@@ -22,15 +22,18 @@
 	console.log("function navBarIntialization() has been fired from navigation.js");
 	*/
 
-//--- Vertical Navigation functionality 
+
 function hideVerticalNavigation(){
     $('#verticalNavItems').slideUp(200);
     $('#navigation').attr('data-vertical_menu_open', false);
 }
+var $navigation = $('#navigation');
+var $openArrow = $('div.quickInfo_open-arrow');
 
-$('#navigation')
+//--- Vertical Navigation functionality 
+$navigation
 	.on('click', 'span.fa-bars', function(){
-	    $('#navigation').attr('data-vertical_menu_open', true);    
+	    $navigation.attr('data-vertical_menu_open', true);    
 	    $('#verticalNavItems').slideDown();
 	    if( $('#quickInfo.ui-panel-open').length ){
 	        $('#quickInfo').panel('close');
@@ -42,24 +45,29 @@ $('#navigation')
 
 
 //--- Quick Info Panel functionality 
-$openArrow = $('div.quickInfo_open-arrow');
-
 $( "body" )
 	.on({
 		panelbeforeopen:function() { 
 			$openArrow.addClass('panel-open');
 			hideVerticalNavigation(); 
 		},
-		panelbeforeclose:function(){ $openArrow.removeClass('panel-open') }
+		panelbeforeclose:function(){ 
+			$openArrow.removeClass('panel-open');
+		},
+		pagecontainerbeforechange:function(){//close Vertical Navigation when navigating to a new page
+			hideVerticalNavigation();
+		}
 	})
 	.on("click","div.quickInfo_close-arrow, div.quickInfo_open-arrow", function(){ 
 		$("#quickInfo").panel( "toggle" );
-	})
+	});
+	//.on("click","#verticalNavItems a", function(){hideVerticalNavigation();})
 
 //--- responsive behavior
 $( window ).resize(function() {
 	var viewPort = $(window).width();
-    if( viewPort > 639 && $('#navigation').data('vertical_menu_open') ){
+	var verticalMenuOpen = $navigation.attr('data-vertical_menu_open') === "true" ? true : false;
+    if( viewPort > 639 && verticalMenuOpen ){
         hideVerticalNavigation()
     }
     if( viewPort > 1280){
