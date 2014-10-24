e1e1f98b2fd8ebc6285fcb76e8dd843e36063e9b
Brice Shatzer <brice.shatzer@gmail.com>
2014-10-24 14:25:53 -0500

    Fixed grammar error

--- data.js
+++ data.js
@@ -108,7 +108,7 @@ var portfolioPrincipal = [
 "tech":["HTML & CSS","LESS","JavaScript","Bootstrap","Lo-Dash","underscore.js","Adobe Business Catalyst"],
 "link":"http://www.marriotttheatre.com/",
 "role":"developer",
-"notes":"The Marriot Theatre needed a solution which allowed them to connect various different collections of information in a way that was coherent and usable for site users. I was responsible for architecting and implementing the solution using <a href='http://businesscatalyst.com/' target='_blank'>Adobe Business Catalyst's</a> custom web app framework.<br> Working around the various quirks and limitations of the platform, I was able to build a system which brought the necessary data to the page in structured manner which enabled the appropriate views to be built on the client side. Below you will find an early design diagram outlining the interactions between the different data collections.",
+"notes":"The Marriot Theatre needed a solution which allowed them to connect various different collections of information in a way that was coherent and usable for site users. I was responsible for architecting and implementing the solution using <a href='http://businesscatalyst.com/' target='_blank'>Adobe Business Catalyst's</a> custom web app framework.<br> Working around the various quirks and limitations of the platform, I was able to build a system which brought the necessary data to the page in a structured manner which enabled the appropriate views to be built on the client side. Below you will find an early design diagram outlining the interactions between the different data collections.",
 "media":["/portfolio/marriott_theater_diagram.png"]
 },
 {"name":"POPAI",
